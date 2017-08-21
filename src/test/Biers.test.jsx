a8ed271989d4df8572e2874cc46fa5b3a6b0e90e
shawkes <shawkes@gmail.com>
2017-08-21 22:47:40 +0100

    Biers snapshot test

---
+++ src/test/Biers.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Biers from '../app/components/Biers.jsx'
+import renderer from 'react-test-renderer';
+
+test('List of Biers', () => {
+  const component = renderer.create(
+    <Biers />,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+})
