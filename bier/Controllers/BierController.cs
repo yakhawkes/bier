d294c2c657d7a9672a382dc2af640d03eec8f1d9
shawkes <shawkes@gmail.com>
2017-08-24 21:56:12 +0100

    BierController hocked up to even more dumb cache

---
+++ bier/Controllers/BierController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Reflection;
+using System.Text;
+using System.Web.Http;
+using log4net;
+
+namespace Bier.Controllers
+{
+    [RoutePrefix("api")]
+    public class BierController : ApiController
+    {
+        private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        [Route("bier")]
+        public HttpResponseMessage GetBeir()
+        {
+            this.Request.GetQueryString();
+            IEnumerable<KeyValuePair<string, string>> queryStringnameValues = this.Request.GetQueryNameValuePairs();
+            string url = $"http://api.brewerydb.com/v2/beers?key=400e2f3f8d21883ddc488e6d964f6cca&{this.Request.GetQueryString()}";
+            var dumbCache = new DumbCache(_log, new HttpWebRequestFactory());
+            try
+            {
+                var responeContent = dumbCache.GetResponse(url);
+                var respons = Request.CreateResponse(HttpStatusCode.OK);
+                respons.Content = new StringContent(responeContent, Encoding.UTF8, "application/json");
+                return respons;
+            }
+            catch (Exception exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, exception.Message, new JsonMediaTypeFormatter());
+            }
+        }
+    }
+}
---
+++ bier/Controllers/RequestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Net.Http;
+
+namespace Bier.Controllers
+{
+    public static class RequestExtensions
+    {
+        public static string GetQueryString(this HttpRequestMessage request)
+        {
+            var v = request.GetQueryNameValuePairs();
+            if (!v.Any()) return "";
+            return request.GetQueryNameValuePairs().Select(x => x.Key + "=" + x.Value).Aggregate((i, j) => i + "&" + j);
+        }
+    }
+}
