cfe68e76dc187e8195adad56394e240c1e9b4273
shawkes <shawkes@gmail.com>
2017-08-24 18:48:49 +0100

    Updating DumbCache on expiry

---
+++ Bier.Tests/DumbCache.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using log4net;
+
+namespace Bier.Tests
+{
+    public class DumbCache
+    {
+        private class CacheEntry
+        {
+            public DateTime Created { get; }
+            public object Data { get; }
+
+            public CacheEntry(DateTime created, object data)
+            {
+                Created = created;
+                Data = data;
+            }
+        }
+        private readonly ILog _logger;
+        private readonly int _cacheTime;
+        private readonly Dictionary<string, CacheEntry> _cache = new Dictionary<string, CacheEntry>();
+
+        public DumbCache(ILog logger, int cacheTime)
+        {
+            _logger = logger;
+            _cacheTime = cacheTime;
+        }
+
+        public DumbCache(ILog logger) : this(logger, 30000)
+        {
+        }
+
+        public string GetResponse(string key)
+        {
+            if (_cache.ContainsKey(key))
+            {
+                if ((DateTime.Now - _cache[key].Created).TotalMilliseconds > _cacheTime)
+                {
+                    _logger.Info("Updating cache");
+                    return _cache[key].Data.ToString();
+                }
+                _logger.Info("Getting from cache");
+                return _cache[key].Data.ToString();
+            }
+            _logger.Info("Adding to cache");
+            _cache.Add(key, new CacheEntry(DateTime.Now, "{\"some\": \"JSON\" }"));
+            return "{\"some\": \"JSON\" }";
+        }
+    }
+}
--- Bier.Tests/DumbCacheTest.cs
+++ Bier.Tests/DumbCacheTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using log4net;
@@ -7,12 +6,13 @@ using NFluent;
 
 namespace Bier.Tests
 {
-    [TestClass] 
+    [TestClass]
     public class UnitTest1
     {
         private List<string> _messages;
         private Mock<ILog> _mockLogger;
         private DumbCache _dumbCache;
+        private DumbCache _dumbBriefCache;
         private string _expectedPayload = "{\"some\": \"JSON\" }";
         private string _theurl = "theURL";
 
@@ -25,7 +25,8 @@ namespace Bier.Tests
             _mockLogger.Setup(m => m.Info(It.IsAny<object>()))
                 .Callback<object>(message => _messages.Add(message.ToString()));
 
-            _dumbCache = new DumbCache(_mockLogger.Object, 1);
+            _dumbCache = new DumbCache(_mockLogger.Object);
+            _dumbBriefCache = new DumbCache(_mockLogger.Object, 1);
         }
 
 
@@ -56,13 +57,13 @@ namespace Bier.Tests
 
             Check.That<string>(response).Equals(_expectedPayload);
             Check.That<string>(response2).Equals(_expectedPayload);
-         }
+        }
         [TestMethod]
         public void DumbCacheLogsInfoTwice()
         {
             string response = _dumbCache.GetResponse(_theurl);
             string response2 = _dumbCache.GetResponse(_theurl);
-            
+
             _mockLogger.Verify(m => m.Info(It.IsAny<object>()), Times.Exactly(2));
         }
         [TestMethod]
@@ -71,32 +72,37 @@ namespace Bier.Tests
             string response = _dumbCache.GetResponse(_theurl);
             string response2 = _dumbCache.GetResponse(_theurl);
 
-           Check.That<string>(_messages[1]).Equals("Getting from cache");
-        }
-    }
-
-    public class DumbCache
-    {
-        private readonly ILog _logger;
-        private readonly int _cacheTime;
-        private readonly Dictionary<string, string> _cache = new Dictionary<string, string>();
-
-        public DumbCache(ILog logger, int cacheTime)
-        {
-            _logger = logger;
-            _cacheTime = cacheTime;
+            Check.That<string>(_messages[1]).Equals("Getting from cache");
         }
 
-        public string GetResponse(string key)
+        [TestMethod]
+        public void DumbCacheReturnsSomeJsonWithSameUrlWithDelay()
         {
-            if (_cache.ContainsKey(key))
-            {
-                _logger.Info("Getting from cache");
-                return _cache[key];
-            }
-            _logger.Info("Adding to cache");
-            _cache.Add(key, "{\"some\": \"JSON\" }");
-            return "{\"some\": \"JSON\" }";
+            string response = _dumbBriefCache.GetResponse(_theurl);
+            System.Threading.Thread.Sleep(1010);
+            string response2 = _dumbBriefCache.GetResponse(_theurl);
+
+            Check.That<string>(response).Equals(_expectedPayload);
+            Check.That<string>(response2).Equals(_expectedPayload);
+        }
+        [TestMethod]
+        public void DumbCacheLogsTwiceWithSameUrlWithDelay()
+        {
+            string response = _dumbBriefCache.GetResponse(_theurl);
+            System.Threading.Thread.Sleep(1010);
+            string response2 = _dumbBriefCache.GetResponse(_theurl);
+
+            _mockLogger.Verify(m => m.Info(It.IsAny<object>()), Times.Exactly(2));
+        }
+        [TestMethod]
+        public void DumbCacheLogsUpdatingWithSameUrlWithDelay()
+        {
+            string response = _dumbBriefCache.GetResponse(_theurl);
+            System.Threading.Thread.Sleep(1010);
+            string response2 = _dumbBriefCache.GetResponse(_theurl);
+
+            Check.That<string>(_messages[0]).Equals("Adding to cache");
+            Check.That<string>(_messages[1]).Equals("Updating cache");
         }
     }
 }
